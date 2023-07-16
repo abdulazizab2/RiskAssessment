96d3e39589164f46c85500048815470ad6c87c90
abdulaziz <albuainaina8@gmail.com>
2023-07-16 23:08:30 +0300

    implement data ingestion

--- RiskAssessment/ingestion.py
+++ RiskAssessment/ingestion.py
@@ -1,26 +1,28 @@
+import subprocess
 import pandas as pd
 import numpy as np
 import os
 import json
 from datetime import datetime
+from RiskAssessment.utils.logger import logging
 
 
+def merge_multiple_dataframe(config):
+    input_folder_path = config["input_folder_path"]
+    output_folder_path = config["output_folder_path"]
+    df = pd.DataFrame()
+    for file in os.listdir(input_folder_path):
+        if file.endswith(".csv"):
+            current_df = pd.read_csv(os.path.join(input_folder_path, file))
+            df = pd.concat([df, current_df])
+    df.drop_duplicates(inplace=True)
+    if not os.path.exists(output_folder_path):
+        os.mkdir(output_folder_path)
+    df.to_csv(os.path.join(output_folder_path, "final_data.csv"))
+    logging.info("SUCCESS: Source data has been ingested and saved to ingested_data/")
 
 
-#############Load config.json and get input and output paths
-with open('config.json','r') as f:
-    config = json.load(f) 
-
-input_folder_path = config['input_folder_path']
-output_folder_path = config['output_folder_path']
-
-
-
-#############Function for data ingestion
-def merge_multiple_dataframe():
-    #check for datasets, compile them together, and write to an output file
-
-
-
-if __name__ == '__main__':
-    merge_multiple_dataframe()
+if __name__ == "__main__":
+    with open("config.json", "r") as f:
+        config = json.load(f)
+    merge_multiple_dataframe(config)
