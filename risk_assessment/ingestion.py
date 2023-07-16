902fe9b32604061b5ec00eb39584a9b8ef089ec0
abdulaziz <albuainaina8@gmail.com>
2023-07-17 00:06:29 +0300

    record ingested data in a text file

--- risk_assessment/ingestion.py
+++ risk_assessment/ingestion.py
@@ -11,14 +11,18 @@ def merge_multiple_dataframe(config):
     input_folder_path = config["input_folder_path"]
     output_folder_path = config["output_folder_path"]
     df = pd.DataFrame()
+    ingested_files = ""
     for file in os.listdir(input_folder_path):
         if file.endswith(".csv"):
             current_df = pd.read_csv(os.path.join(input_folder_path, file))
             df = pd.concat([df, current_df])
+            ingested_files += file + "\n"
     df.drop_duplicates(inplace=True)
     if not os.path.exists(output_folder_path):
         os.mkdir(output_folder_path)
     df.to_csv(os.path.join(output_folder_path, "final_data.csv"))
+    with open(os.path.join(output_folder_path, "ingested_files.txt"), "w") as f:
+        f.write(ingested_files)
     logging.info("SUCCESS: Source data has been ingested and saved to ingested_data/")
 
 
