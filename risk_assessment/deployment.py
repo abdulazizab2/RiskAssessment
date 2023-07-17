f92319525dc7585979389ebc931307814f0ef7f0
abdulaziz <albuainaina8@gmail.com>
2023-07-17 23:42:57 +0300

    implement deployment script

--- risk_assessment/deployment.py
+++ risk_assessment/deployment.py
@@ -1,27 +1,32 @@
-from flask import Flask, session, jsonify, request
 import pandas as pd
 import numpy as np
-import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+import shutil
 import json
+from risk_assessment.utils.logger import logging
 
 
+def copy_files_to_deployment_directory(files, deployment_path):
+    if not os.path.exists(deployment_path):
+        os.mkdir(deployment_path)
+    for file in files:
+        shutil.copy(file, deployment_path)
+    logging.info("SUCCESS: Model is deployed")
 
-##################Load config.json and correct path variable
-with open('config.json','r') as f:
-    config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-prod_deployment_path = os.path.join(config['prod_deployment_path']) 
+def main():
+    with open("config.json", "r") as f:
+        config = json.load(f)
+
+    dataset_csv_path = os.path.join(config["output_folder_path"])
+    model_path = os.path.join(config["output_model_path"])
+    prod_deployment_path = os.path.join(config["prod_deployment_path"])
+    files = []
+    files.append(os.path.join(dataset_csv_path, "ingested_files.txt"))
+    files.append(os.path.join(model_path, "trained_model.pkl"))
+    files.append(os.path.join(model_path, "latest_score.txt"))
+    copy_files_to_deployment_directory(files, deployment_path=prod_deployment_path)
 
 
-####################function for deployment
-def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
-
+if __name__ == "__main__":
+    main()
