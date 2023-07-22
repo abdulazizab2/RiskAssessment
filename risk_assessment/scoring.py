0893eec257b114fe6125b9deb110065a039e14c4
abdulaziz <albuainaina8@gmail.com>
2023-07-22 17:23:06 +0300

    minor refactoring

--- risk_assessment/scoring.py
+++ risk_assessment/scoring.py
@@ -8,13 +8,15 @@ from risk_assessment.utils.logger import logging
 from risk_assessment.common import preprocess_data
 
 
-def score_model(model, data, results_path):
+def score_model(model, data, results_path=None, save_result=True):
     X, y = data
     preds = model.predict(X)
     f1_score_ = f1_score(y, preds)
-    with open(os.path.join(results_path, "latest_score.txt"), "w") as f:
-        f.write(f"{f1_score_:0.4f}")
-    logging.info(f"SUCCESS: Model score is saved in {results_path}/ directory")
+    if save_result:
+        with open(os.path.join(results_path, "latest_score.txt"), "w") as f:
+            f.write(f"{f1_score_:0.4f}")
+        logging.info(f"SUCCESS: Model score is saved in {results_path}/ directory")
+    return f1_score_
 
 
 def main():
